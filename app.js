01ed4e4eaa9c0236594b4cdb44ce56437ca64983
RAHAT <96333576+bazarbairahat@users.noreply.github.com>
2023-01-13 23:31:18 +0600

    Work

---
+++ app.js
@@ -0,0 +1,34 @@
+$(function() {
+
+/* Fixed Header */
+let header = $("#header");
+let intro = $("#intro");
+let introH = intro.innerHeight();
+let scrollPos = $(window).scrollTop();
+
+$(window).on("scroll load resize", function() {
+introH = intro.innerHeight();
+scrollPos = $(this).scrollTop();
+
+if( scrollPos > introH) {
+header.addClass("fixed");
+} else {
+	header.removeClass("fixed");
+}
+});
+
+/* Smooth scroll */
+$("data-scroll]").on("click", function(event) {
+	event.preventDefault();
+
+	let elementId = $(this).data('scroll');
+	let elementOffset = $(elementId).offset().top;
+	
+	$("html, baby").animate({
+		scrollTop: elementOffset
+	});
+
+});
+
+
+});
